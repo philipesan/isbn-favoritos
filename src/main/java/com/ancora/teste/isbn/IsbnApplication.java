a8eeef66a5619f1e305d43b028ce176ab674f076
Victor Philipe Arakaki <philipesan123@hotmail.com>
2024-03-16 14:14:32 -0300

    commit inicial do projeto

---
+++ src/main/java/com/ancora/teste/isbn/IsbnApplication.java
@@ -0,0 +1,13 @@
+package com.ancora.teste.isbn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsbnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ancora/teste/isbn/IsbnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ancora.teste.isbn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsbnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
